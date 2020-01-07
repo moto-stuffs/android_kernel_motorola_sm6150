ce058214f347007c1ad536500b90c6d3cacdac0a
Mohammed Nayeem Ur Rahman <mohara@codeaurora.org>
2020-01-07 17:07:55 +0530

    msm: adsprpc: Variable map may UAF due to race conditions
    Variable map may pointing to the same buffer on race conditions
    in functions fastrpc_internal_mmap, fastrpc_internal_munmap and,
    fastrpc_internal_munmap_fd. Use mutex to avoid race conditions
    on same buffer.
    
    Mot-CRs-fixed: (CR)
    CVE-Fixed: CVE-2020-3680
    CRs-Fixed: 2597382
    
    Change-Id: Ic51bce81a92b68516c9bdcf4f9955a06392d75e8
    Acked-by: Krishnaiah Tadakamalla <ktadakam@qti.qualcomm.com>
    Signed-off-by: Signed-off-by: Mohammed Nayeem Ur Rahman <mohara@codeaurora.org>
    (cherry picked from commit fc548a5919c8c381afe283e1385fdff70e83ac6f)
    Signed-off-by: Jignesh Patel <jignesh@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1511162
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Ravikumar Vembu <raviv@motorola.com>
    Submit-Approved: Jira Key

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -2959,6 +2959,7 @@ static int fastrpc_internal_munmap_fd(struct fastrpc_file *fl,
 			(unsigned int)ud->len);
 		err = -1;
 		mutex_unlock(&fl->map_mutex);
+		mutex_unlock(&fl->internal_map_mutex);
 		goto bail;
 	}
 	if (map && (map->attr & FASTRPC_ATTR_KEEP_MAP)) {
@@ -2966,6 +2967,7 @@ static int fastrpc_internal_munmap_fd(struct fastrpc_file *fl,
 		fastrpc_mmap_free(map, 0);
 	}
 	mutex_unlock(&fl->map_mutex);
+	mutex_unlock(&fl->internal_map_mutex);
 bail:
 	mutex_unlock(&fl->internal_map_mutex);
 	return err;

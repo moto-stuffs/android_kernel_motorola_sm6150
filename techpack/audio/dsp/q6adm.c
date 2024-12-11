a98ffab5f8bbedf1d189c18c8bac721f2f9aa5aa
Vhmit <viktor.9630@protonmail.com>
2024-12-10 22:13:17 -0300

    techpack: Modify/adjust for Motorola SM6150
    Change-Id: I6b29516f4771b1035f79ff10db17aeea9badb7f2
    Signed-off-by: Vhmit <viktor.9630@protonmail.com>

--- techpack/audio/dsp/q6adm.c
+++ techpack/audio/dsp/q6adm.c
@@ -1,5 +1,4 @@
 /* Copyright (c) 2012-2020, The Linux Foundation. All rights reserved.
- * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -1563,7 +1562,7 @@ static int32_t adm_callback(struct apr_client_data *data, void *priv)
 {
 	uint32_t *payload;
 	int port_idx, copp_idx, idx, client_id;
-	uint32_t num_modules;
+	int num_modules;
 	int ret;
 
 	if (data == NULL) {
